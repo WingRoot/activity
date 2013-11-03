0a2c5eabf0cca5d37257818f69d1a71edfdddc34
Luiz Junqueira <junqueira.luizcarlo@gmail.com>
2013-11-03 13:17:43 -0200

    Controle de atividade com metodo edit, update e delete.

--- app/controllers/atividades_controller.rb
+++ app/controllers/atividades_controller.rb
@@ -2,11 +2,37 @@
 
 class AtividadesController < ApplicationController
 
-  def create 
+  def create
   	atividade = Atividade.new params[:atividade]
 
   	if atividade.save
   		flash[:notice] = "Sua atividade foi cadastrada."
+  	end
+
+  	redirect_to root_path
+  end
+
+  def edit
+  	@atividade = Atividade.find params[:id]
+  end
+
+  def update
+  	atividade = Atividade.find params[:id]
+  	if atividade.update_attributes params[:atividade]
+  		flash[:notice] = "A atividade foi atualizada."
+  	end
+
+  	redirect_to root_path
+  end
+
+  def delete
+  	@atividade = Atividade.find(params[:id])
+  end
+
+  def destroy
+  	atividade = Atividade.find(params[:id])
+  	if atividade.destroy
+  		flash[:notice] = "A atividade foi excluida."
   	end
 
   	redirect_to root_path
